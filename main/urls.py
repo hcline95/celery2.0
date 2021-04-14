e025515f6ca6b6c20a9e31601c15113ede3c30e6
Hannah Cline <hannah@visimo.ai>
2021-04-14 11:58:55 -0400

    first commit

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views 
+
+app_name = "main"
+
+urlpatterns = [
+    path("", views.homepage, name="homepage"),
+    path("tasks", views.tasks, name="tasks"),
+    path('check', views.check_task_status, name='check_task_status')
+]
---
+++ main/views.py
@@ -0,0 +1,22 @@
+import redis
+from django.shortcuts import render
+from django.http import HttpResponse
+from celery.result import AsyncResult
+from .tasks import my_first_task
+redis_cache = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+
+# Create your views here.
+def homepage(request): 
+	return HttpResponse("<h4>Hello World</h4>")	
+
+
+def tasks(request): 
+	response = my_first_task.delay(10)
+	redis_cache.set('task1', response.id)
+	ready = response.ready()
+	return HttpResponse(ready)	
+
+def check_task_status(request):
+    task_id = redis_cache.get('task1')
+    return AsyncResult(task_id).status
